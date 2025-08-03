69b0074afdc2dbf5d597812ea6043b1211076a41
Sarthak Vyas <sarthakvyas41@gmail.com>
2025-08-03 17:47:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (15.64%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -1,27 +1,33 @@
 class Solution {
+    vector<int> shift(vector<int>& help, int k){
+        vector<int> ansHelp(help.size());
+        for(int j=0; j<help.size(); j++){
+            ansHelp[(j+k)%(help.size())] = help[j];
+        }
+        return ansHelp;
+    }
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        if(k==0) return grid;
         int r=grid.size();
         int c=grid[0].size();
-        int count=0;
 
-        while(count<k){
-            vector<int>help;
-            help.push_back(grid[r-1][c-1]);
+        vector<int>help;
+        //help.push_back(grid[r-1][c-1]);(for once!!!)
 
-            for(int i=0; i<r ;i++){
-                for(int j=0; j<c ;j++){
-                    if(i==r-1 && j==c-1) continue;
-                    help.push_back(grid[i][j]);
-                }
+        for(int i=0; i<r ;i++){
+            for(int j=0; j<c ;j++){
+                //if(i==r-1 && j==c-1) continue;
+                help.push_back(grid[i][j]);
             }
-            int k=0;
-            for(int i=0; i<r ;i++){
-                for(int j=0; j<c ;j++){
-                    grid[i][j]=help[k++];
-                }
+        }
+        help=shift(help, k);
+                   
+        int n=0;
+        for(int i=0; i<r ;i++){
+            for(int j=0; j<c ;j++){
+                grid[i][j]=help[n++];
             }
-            count++;
         }
         return grid;
     }
