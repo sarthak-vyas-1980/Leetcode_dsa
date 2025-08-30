087a09002d636cf3d149ced901994a1a06b82cae
Sarthak Vyas <sarthakvyas41@gmail.com>
2025-08-30 20:15:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.01%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return x;
+        }
+        if(n==-1){
+            return 1/x;
+        }
+        double a=myPow(x,n/2);
+        if(n%2==0){
+            return a*a;
+        } else {
+            if(n>0){
+                return x*a*a;
+            } else {
+                return (1/x)*a*a;
+            }
+        }
+    }
+};
