ba53a72f89b0907d8bf6346543f5b95d9b7ef699
Sarthak Vyas <sarthakvyas41@gmail.com>
2025-10-31 00:15:04 +0530

    Time: 11 ms (42.75%) | Memory: 17.2 MB (26.00%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,20 +1,38 @@
+// class Solution {
+// public:
+//     int findPairs(vector<int>& nums, int k) {
+//         sort(nums.begin(), nums.end());
+        
+//         int ans = 0;
+//         int i = 0;
+//         while(i<nums.size()){
+//             for(int j=i+1; j<nums.size(); j++){
+//                 if(nums[i]-nums[j]==k || nums[j]-nums[i]==k){
+//                     ans++;
+//                     break;
+//                 }
+//             }
+//             int j=i+1;
+//             while(j<nums.size() && nums[j]==nums[i]) j++;
+//             i=j;
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        
+        unordered_map<int, int> count1;
+        for(int i=0; i<nums.size(); i++){
+            count1[nums[i]]++;
+        }
         int ans = 0;
-        int i = 0;
-        while(i<nums.size()){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i]-nums[j]==k || nums[j]-nums[i]==k){
-                    ans++;
-                    break;
-                }
+        for(auto i: count1){
+            if(k==0){
+                if(i.second>1) ans++;
+            } else {
+                if(count1.count(i.first+k)) ans++;
             }
-            int j=i+1;
-            while(j<nums.size() && nums[j]==nums[i]) j++;
-            i=j;
         }
         return ans;
     }
