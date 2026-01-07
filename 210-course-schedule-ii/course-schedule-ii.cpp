a747737ce280bfbfb281d7a13ea2e42c60f5cf40
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-01-08 02:34:14 +0530

    Time: 12 ms (9.72%) | Memory: 20.3 MB (5.01%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, list<int>> adj;
+        for(int i=0; i<prerequisites.size(); i++){
+            int u = prerequisites[i][1];
+            int v = prerequisites[i][0];
+            adj[u].push_back(v);
+        }
+        unordered_map<int, int> inedge(numCourses);
+        for(auto i: adj){
+            for(auto j: i.second){
+                inedge[j]++;
+            }
+        }
+        int count = 0;
+        queue<int> q;
+        vector<int> ans;
+        vector<int> dummy;
+        for(int i=0; i<numCourses; i++){
+            if(inedge[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            count++;
+            ans.push_back(front);
+            for(auto i: adj[front]){
+                inedge[i]--;
+                if(inedge[i]==0) q.push(i);
+            }
+        }
+        if(count == numCourses) return ans;
+        return dummy;
+    }
+};
