06153c6068d9cbd0fffda9870c9095dde8358428
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-01-17 18:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (62.96%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int, int> count;
+        for(int i=0; i<nums.size(); i++){
+            count[nums[i]]++;
+        }
+        int sum = 0;
+        for(auto i: count){
+            if(i.second==1) sum += i.first;
+        }
+        return sum;
+    }
+};
