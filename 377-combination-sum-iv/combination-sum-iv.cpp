b6c17073bc74a33048ba6a16d47e1529d34b6fed
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-02-02 02:59:41 +0530

    Time: 4 ms (29.26%) | Memory: 9.1 MB (90.39%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<long long> dp(target + 1, 0);
+        dp[0] = 1;
+
+        for (int j = 1; j <= target; j++) {
+            for (int i = 0; i < nums.size(); i++) {
+                if (j - nums[i] >= 0) {
+                    dp[j] += dp[j - nums[i]];
+                    if (dp[j] > INT_MAX) {
+                        dp[j] = INT_MAX; // clamp to prevent overflow
+                    }
+                }
+            }
+        }
+        return (int)dp[target];
+    }
+};
