a0e3e120c42afcf97981339cdb619633c20f436a
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-02-06 01:05:02 +0530

    Time: 33 ms (40.87%) | Memory: 39 MB (19.13%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,23 +1,34 @@
 class Solution {
-    int solve(vector<int>& nums, int target, int sum, int index){
-        if(index==-1 && sum==target) return 1;
-        if(index==-1) return 0;
-
-        int add = solve(nums, target, sum+nums[index], index-1);
-        int sub = solve(nums, target, sum-nums[index], index-1);
-        return add+sub;
-    }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums, target, 0, nums.size()-1);
-        // vector<int> curr(nums.size()+1, 0);
-        // vector<int> prev(nums.size()+1, 0);
-
-        // prev[target]
-        // for(int i=nums.size()-1; i>=0; i--){
-        //     int sum = 0; 
-        //     for8
-        // }
-
+        int n = nums.size(), sum = accumulate(nums.begin(), nums.end(), 0);
+        int ofs = 2*sum;
+        if(abs(target) > sum) return 0;
+        vector<vector<int>> dp(n+1, vector<int>(2*ofs + 1,0));
+        dp[0][ofs] = 1; //think this as starting node(0,0) as we start things from ofs 
+        for(int i=1; i<=n; i++) {
+            for(int j=-sum; j<=sum; j++) {
+                dp[i][j + ofs] += dp[i-1][j + nums[i-1] + ofs];
+                dp[i][j + ofs] += dp[i-1][j - nums[i-1] + ofs];
+                // dp[i][j + nums[i-1] + ofs] += dp[i-1][j + ofs];
+                // dp[i][j - nums[i-1] + ofs] += dp[i-1][j + ofs];
+            }
+        }
+        return dp[n][target + ofs];
     }
 };
+
+// class Solution {
+//     int solve(vector<int>& nums, int target, int sum, int index){
+//         if(index==-1 && sum==target) return 1;
+//         if(index==-1) return 0;
+
+//         int add = solve(nums, target, sum+nums[index], index-1);
+//         int sub = solve(nums, target, sum-nums[index], index-1);
+//         return add+sub;
+//     }
+// public:
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         return solve(nums, target, 0, nums.size()-1);
+//     }
+// };
