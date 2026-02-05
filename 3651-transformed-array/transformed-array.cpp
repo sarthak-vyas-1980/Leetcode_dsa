5403abdf024fe645fdf939be3441f988ef7480ff
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-02-05 18:58:11 +0530

    Time: 4 ms (79.67%) | Memory: 25.9 MB (8.08%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0 || n==1) return nums;
+        vector<int> result(nums.size());
+        for(int i=0; i<n; i++){
+            int x = nums[i];
+            if(x==0) result[i]=x;
+            else if(x>0) result[i] = nums[(i+x)%(n)];
+            else {
+                int y = abs(x)%n;
+                if((i-y)<0){
+                    result[i] = nums[n+i-y];
+                }
+                else result[i] = nums[i-y];
+            }
+            // int x = nums[i];
+            // if(i+x<0) result[i] = nums[(n+i+x)%n];
+            // else result[i] = nums[(i+x)%(n)];
+        }
+        return result;
+    }
+};
