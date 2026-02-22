f3181380e0867f01dd9370a6d84c5b1dde717dc8
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-02-23 01:09:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (94.71%) - LeetSync

--- 899-binary-gap/binary-gap.cpp
+++ 899-binary-gap/binary-gap.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     int binaryGap(int n) {
-        string s;
+        int count = INT_MIN;
+        int ans = 0;
         while(n!=0){
-            s.push_back(n%2 + '0');
+            char ch = n%2 + '0';
+            if(ch == '1') {
+                ans = max(ans, count);
+                count = 1;
+            } else{
+                count++;
+            } 
             n /= 2;
         }
-        int lastIndex = -1e9;
-        int ans = 0;
-        for(int i=s.length()-1; i>=0; i--){
-            if(s[i] == '1') {
-                ans = max(ans, lastIndex-i);
-                lastIndex = i;
-            }
-        }
-        return ans <= -1e9 ? 0 : ans;
+        return ans;
     }
+    // int binaryGap(int n) {.............................
+    //     string s;
+    //     while(n!=0){
+    //         s.push_back(n%2 + '0');
+    //         n /= 2;
+    //     }
+    //     int lastIndex = -1e9;
+    //     int ans = 0;
+    //     for(int i=s.length()-1; i>=0; i--){
+    //         if(s[i] == '1') {
+    //             ans = max(ans, lastIndex-i);
+    //             lastIndex = i;
+    //         }
+    //     }
+    //     return ans <= -1e9 ? 0 : ans;
+    // }
 };
