d10b825c7758400c07c2217d8a3a5976d18563e5
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-02-23 01:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (44.66%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        string s;
+        while(n!=0){
+            s.push_back(n%2 + '0');
+            n /= 2;
+        }
+        int lastIndex = -1e9;
+        int ans = 0;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s[i] == '1') {
+                ans = max(ans, lastIndex-i);
+                lastIndex = i;
+            }
+        }
+        return ans <= -1e9 ? 0 : ans;
+    }
+};
