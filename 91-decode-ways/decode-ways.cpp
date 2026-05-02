69ebe3e068e564033fd9d31c83aaaa7a5f0b160a
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-05-02 12:00:39 +0530

    Time: 2 ms (23.30%) | Memory: 8.2 MB (95.63%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int solve(string& s, int index, vector<int>& dp) {
+    int solve(string& s, int index, vector<int>& dp) {  //Top-down Solution
         if (index < 0) return 1;  // valid decoding
         if (dp[index] != -1) return dp[index];
 
@@ -18,22 +18,42 @@ class Solution {
 public:
     int numDecodings(string s) {
         if(s[0]=='0') return 0;
-        vector<int> dp(s.length()+1, 0);
-        dp[0] = 1;
+        int two = 1, one = 1;
         
         for(int index=1; index<=s.length(); index++){
-            int ways = 0;
+            int curr = 0;
             if (s[index-1] != '0') {
-                ways += dp[index-1];
+                curr += one;
             }
             if (index > 1) {  //index-2 >= 0
                 int num = (s[index - 2] - '0') * 10 + (s[index-1] - '0');
                 if (num >= 10 && num <= 26) {
-                    ways += dp[index-2];
+                    curr += two;
                 }
             }
-            dp[index] = ways;
+            two = one;
+            one = curr;
         }
-        return dp[s.length()];
+        return one;
+
+        //Tabulation:
+        // if(s[0]=='0') return 0;
+        // vector<int> dp(s.length()+1, 0);
+        // dp[0] = 1;
+        
+        // for(int index=1; index<=s.length(); index++){
+        //     int ways = 0;
+        //     if (s[index-1] != '0') {
+        //         ways += dp[index-1];
+        //     }
+        //     if (index > 1) {  //index-2 >= 0
+        //         int num = (s[index - 2] - '0') * 10 + (s[index-1] - '0');
+        //         if (num >= 10 && num <= 26) {
+        //             ways += dp[index-2];
+        //         }
+        //     }
+        //     dp[index] = ways;
+        // }
+        // return dp[s.length()];
     }
 };
