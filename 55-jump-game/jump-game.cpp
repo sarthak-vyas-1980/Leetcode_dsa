714a4907be3c58ce5b1f7968ef1820b3a0f64f36
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-05-25 19:29:47 +0530

    Time: 1 ms (30.90%) | Memory: 52.3 MB (56.81%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,39 +1,12 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums){
-        int n = nums.size();
-        vector<int> dp(n, 0);
-        dp[n-1] = 1;
+    bool canJump(vector<int>& nums) {
+        int farthest = 0;
 
-        for(int i=n-2; i>=0; i--){
-            bool ans = 0;
-            for(int k=nums[i]; k>0; k--){
-                if(k+i < nums.size()) ans = dp[i+k];
-                if(ans) break;
-            }
-            dp[i] = ans;
+        for(int i=0; i<nums.size(); i++){
+            if(i > farthest) return false;
+            farthest = max(farthest, i + nums[i]);
         }
-        return dp[0];
+        return true;
     }
 };
-// class Solution {
-//     bool solve(vector<int> &nums, int index, vector<int>& dp){
-//         if(index==nums.size()-1) return 1;
-//         if(dp[index]!=-1) return dp[index];
-
-//         bool ans = 0;
-//         for(int i=nums[index]; i>0; i--){
-//             if(i+index < nums.size()) ans = solve(nums, index+i, dp);
-//             if(ans) break;
-//         }
-//         dp[index] = ans;
-//         return dp[index];
-//     }
-// public:
-//     bool canJump(vector<int>& nums){
-//         int index = 0; 
-//         vector<int> dp(nums.size(), -1);
-//         solve(nums, index, dp);
-//         return dp[0];
-//     }
-// };
