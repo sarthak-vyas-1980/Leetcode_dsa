3a162527d96c1e056f287930b85edcbfafefe04f
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-05-25 18:58:16 +0530

    Time: 89 ms (14.65%) | Memory: 21.6 MB (15.31%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
-    int solve(vector<int>& nums, int index, vector<int>& dp){
+    int solveMem(vector<int>& nums, int index, vector<int>& dp){
         if(index == nums.size()-1) return 0;
         if(index >= nums.size()) return INT_MAX;
         if(dp[index] != -1) return dp[index];
 
         int ans = INT_MAX;
         for(int i=1; i<=nums[index]; i++){
-            int jump = solve(nums, index+i, dp);
+            int jump = solveMem(nums, index+i, dp);
             if(jump != INT_MAX){
                 ans = min(ans, jump + 1);
             }
@@ -16,7 +16,18 @@ class Solution {
     }
 public:
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size()+1, - 1);
-        return solve(nums, 0, dp);
+        // vector<int> dp(nums.size()+1, - 1);
+        // return solveMem(nums, 0, dp);
+
+        vector<int> dp(nums.size(), INT_MAX);
+        dp[nums.size()-1] = 0;
+
+        for(int index=nums.size()-2; index>=0; index--){
+            for(int i=1; i<=nums[index]; i++){
+                if(i + index >= nums.size() || dp[index + i] == INT_MAX) continue;
+                dp[index] = min(dp[index], dp[index + i] + 1);
+            }
+        }
+        return dp[0];
     }
 };
