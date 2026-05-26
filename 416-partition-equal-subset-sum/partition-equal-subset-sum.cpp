6acf6657d5d5c6eb7754c869e243c6bcb53744e5
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-05-26 22:21:08 +0530

    Time: 159 ms (28.14%) | Memory: 17.4 MB (63.38%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,11 +1,11 @@
 class Solution {
-    bool solve(vector<int>& nums, int& sum, int index, int val, vector<vector<int>>& dp){
+    bool solveMem(vector<int>& nums, int& sum, int index, int val, vector<vector<int>>& dp){
         if(index >= nums.size() && sum/2 == val) return true;
         if(index >= nums.size() && sum/2 != val) return false;
         if(dp[index][val] != -1) return dp[index][val];
 
-        bool take = solve(nums, sum, index+1, val + nums[index], dp); 
-        bool notTake = solve(nums, sum, index+1, val, dp); 
+        bool take = solveMem(nums, sum, index+1, val + nums[index], dp); 
+        bool notTake = solveMem(nums, sum, index+1, val, dp); 
         return dp[index][val] = take || notTake;
     }
 public:
@@ -14,7 +14,20 @@ public:
         for(int num: nums) sum += num;
         if(sum%2 == 1) return false;
 
-        vector<vector<int>> dp(nums.size()+1, vector<int>(sum+1, -1));
-        return solve(nums, sum, 0, 0, dp);
+        // vector<vector<int>> dp(nums.size()+1, vector<int>(sum+1, -1));
+        // return solveMem(nums, sum, 0, 0, dp);
+
+        vector<vector<bool>> dp(nums.size()+1, vector<bool>((sum/2)+1, 0));
+        for(int i=0; i<=nums.size(); i++) dp[i][0] = 1;
+
+        for(int index=nums.size()-1; index>=0; index--){
+            for(int val=1; val<=sum/2; val++){ 
+                bool take = 0;
+                if(val-nums[index]>=0) take = dp[index+1][val-nums[index]];
+                bool notTake = dp[index+1][val];
+                dp[index][val] = take || notTake;
+            }
+        }
+        return dp[0][sum/2];
     }
 };
