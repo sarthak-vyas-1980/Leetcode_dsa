a2c1cfe29f5c9aa45eabe5eebc8c062f80af2e72
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-05-26 17:34:17 +0530

    Time: 12 ms (39.54%) | Memory: 27.1 MB (7.74%) - LeetSync

--- 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
+++ 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
@@ -1,16 +1,35 @@
 class Solution {
-    int solve(vector<int>& slices, int n, int index, int endInd, vector<vector<int>>& dp){
+    int solveMem(vector<int>& slices, int n, int index, int endInd, vector<vector<int>>& dp){
         if(n == 0 || index > endInd) return 0;
         if(dp[index][n] != -1) return dp[index][n];
 
-        int take = slices[index] + solve(slices, n-1, index + 2, endInd, dp);
-        int notTake = 0 + solve(slices, n, index + 1, endInd, dp);
+        int take = slices[index] + solveMem(slices, n-1, index + 2, endInd, dp);
+        int notTake = 0 + solveMem(slices, n, index + 1, endInd, dp);
         return dp[index][n] = max(take, notTake);
     }
 public:
     int maxSizeSlices(vector<int>& slices) {
+        // int n = slices.size();
+        // vector<vector<int>> dp1(n+2, vector<int>(n+2, -1)), dp2(n+2, vector<int>(n+2, -1));
+        // return max(solveMem(slices, n/3, 0, n-2, dp1), solveMem(slices, n/3, 1, n-1, dp2));
+
         int n = slices.size();
-        vector<vector<int>> dp1(n+2, vector<int>(n+2, -1)), dp2(n+2, vector<int>(n+2, -1));
-        return max(solve(slices, n/3, 0, n-2, dp1), solve(slices, n/3, 1, n-1, dp2));
+        vector<vector<int>> dp1(n+2, vector<int>(n+2, 0)), dp2(n+2, vector<int>(n+2, 0));
+
+        for(int index=n-2; index>=0; index--){
+            for(int k=1; k<=n/3; k++){
+                int take = slices[index] + dp1[index+2][k-1];
+                int notTake = 0 + dp1[index+1][k];
+                dp1[index][k] = max(take, notTake);
+            }
+        }
+        for(int index=n-1; index>=1; index--){
+            for(int k=1; k<=n/3; k++){
+                int take = slices[index] + dp2[index+2][k-1];
+                int notTake = 0 + dp2[index+1][k];
+                dp2[index][k] = max(take, notTake);
+            }
+        }
+        return max(dp1[0][n/3], dp2[1][n/3]);
     }
 };
