8da31b0086d3e33580e4f415cea41ec729f11f4b
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-05-28 20:09:04 +0530

    Time: 78 ms (51.56%) | Memory: 124.7 MB (40.00%) - LeetSync

--- 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
+++ 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
@@ -16,7 +16,23 @@ public:
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.begin(), -1);
         nums2.insert(nums2.begin(), -1);
-        vector<vector<int>> dp(nums1.size()+1, vector<int>(2, -1));
-        return solveMem(nums1, nums2, 1, 0, dp);
+        // vector<vector<int>> dp(nums1.size()+1, vector<int>(2, -1));
+        // return solveMem(nums1, nums2, 1, 0, dp);
+
+        vector<vector<int>> dp(nums1.size()+1, vector<int>(2, 0));
+        
+        for(int index=nums1.size()-1; index>0; index--){
+            for(int swapped= 1; swapped>=0; swapped--){
+                int prev1 = nums1[index-1];
+                int prev2 = nums2[index-1];
+                if(swapped) swap(prev1, prev2);
+
+                int ans = INT_MAX; 
+                if(nums1[index] > prev1 && nums2[index] > prev2) ans = dp[index+1][0];
+                if(nums1[index] > prev2 && nums2[index] > prev1) ans = min(ans, 1 + dp[index+1][1]);
+                dp[index][swapped] = ans;
+            }
+        }
+        return dp[1][0];
     }
 };
