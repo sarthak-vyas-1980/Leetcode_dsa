c35d8fa303043b20dd94704b56203ac9314e19c1
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-06-04 21:55:43 +0530

    Time: 12 ms (70.33%) | Memory: 18.8 MB (91.95%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -19,25 +19,34 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(!root) return root;
-        queue<Node*> q;
-        q.push(root);
+        if(!root) return NULL;
 
-        while(!q.empty()){
-            int size = q.size();
-            Node* nextNode = NULL;
+        if(root->left) root->left->next = root->right;
+        if(root->right && root->next) root->right->next = root->next->left;
+        connect(root->left);
+        connect(root->right);
 
-            for(int i=0; i<size; i++){
-                Node* temp = q.front();
-                q.pop();
-
-                temp->next = nextNode;
-                nextNode = temp;
-                
-                if(temp->right) q.push(temp->right);
-                if(temp->left) q.push(temp->left);
-            }
-        }
         return root;
+
+        // if(!root) return root;
+        // queue<Node*> q;
+        // q.push(root);
+
+        // while(!q.empty()){
+        //     int size = q.size();
+        //     Node* nextNode = NULL;
+
+        //     for(int i=0; i<size; i++){
+        //         Node* temp = q.front();
+        //         q.pop();
+
+        //         temp->next = nextNode;
+        //         nextNode = temp;
+                
+        //         if(temp->right) q.push(temp->right);
+        //         if(temp->left) q.push(temp->left);
+        //     }
+        // }
+        // return root;
     }
 };
