017e930fe173831bb46053048620c00a2a303a36
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-06-06 03:38:16 +0530

    Time: 59 ms (38.53%) | Memory: 54.6 MB (14.94%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -17,7 +17,23 @@ class Solution {
     }
 public:
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.length()+1, vector<int>(p.length()+1, -1));
-        return solve(s, p, s.length(), p.length(), dp);
+        // vector<vector<int>> dp(s.length()+1, vector<int>(p.length()+1, -1));
+        // return solve(s, p, s.length(), p.length(), dp);
+
+        vector<vector<int>> dp(s.length()+1, vector<int>(p.length()+1, 0));
+        dp[0][0] = 1;
+
+        for(int i=1; i<=p.length(); i++){
+            dp[0][i] = dp[0][i-1] && p[i-1] == '*';
+        }
+
+        for(int i=1; i<=s.length(); i++){
+            for(int j=1; j<=p.length(); j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*') dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                else dp[i][j] = false;
+            }
+        }
+        return dp[s.length()][p.length()];
     }
 };
