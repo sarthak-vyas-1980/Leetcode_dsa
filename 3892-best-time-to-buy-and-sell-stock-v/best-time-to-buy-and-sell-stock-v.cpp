508885335fd1ffc9f422a2a523cae067dd311d5f
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-06-07 14:04:09 +0530

    Time: 1714 ms (5.03%) | Memory: 503.3 MB (5.86%) - LeetSync

---
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    long long solve(int index, int opr, bool isShort, vector<int>& prices, int k, vector<vector<vector<long long>>>& dp){
+        if(index == prices.size()){
+            if(opr % 2 == 0) return 0;   // no open position
+            return -1e15;                // open position left
+        }
+        if(opr == 2*k) return 0;
+        if(dp[index][opr][isShort] != -1) return dp[index][opr][isShort];
+        
+        long long profit = 0;
+        if(opr%2 == 0) {  //buy and sell both allowed
+            long long buyKaro = -prices[index] + solve(index+1, opr+1, 0, prices, k, dp);
+            long long skipKaro = 0 + solve(index+1, opr, 0, prices, k, dp);
+            long long sellKaro = prices[index] + solve(index+1, opr+1, 1, prices, k, dp);
+            profit = max(buyKaro, max(skipKaro, sellKaro));
+        }
+        else {
+            if(isShort == 0){
+                long long sellKaro = prices[index] + solve(index+1, opr+1, 0, prices, k, dp);
+                long long skipKaro = 0 + solve(index+1, opr, 0, prices, k, dp);
+                profit = max(sellKaro, skipKaro);
+            }
+            else{
+                long long buyKaro = -prices[index] + solve(index+1, opr+1, 1, prices, k, dp);
+                long long skipKaro = 0 + solve(index+1, opr, 1, prices, k, dp);
+                profit = max(buyKaro, skipKaro);
+            }
+        }
+        return dp[index][opr][isShort] = profit;
+    }
+public:
+    long long maximumProfit(vector<int>& prices, int k) {
+        int n = prices.size();
+        vector<vector<vector<long long>>> dp(n, vector<vector<long long>>(2*k+1, vector<long long>(2, -1)));
+        return solve(0, 0, 0, prices, k, dp);
+    }
+};
