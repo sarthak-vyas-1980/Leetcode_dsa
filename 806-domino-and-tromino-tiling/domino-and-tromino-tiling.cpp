01615a957e9b096e1395f3316cc6707b3bb2f38f
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-07-20 00:53:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (36.78%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -15,7 +15,23 @@ class Solution {
     }
 public:
     int numTilings(int n) {
-        vector<long long> dp(n+1, -1);
-        return solve(n, dp);
+        // vector<long long> dp(n+1, -1);
+        // return solve(n, dp);
+
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        if(n == 3) return 5;
+        vector<long long> dp(n+1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+
+        for(int i=4; i<=n; i++){
+            long long one = (2 * dp[i-1]) % mod;
+            long long three = dp[i-3] % mod;
+            dp[i] = (one + three) % mod;
+        }
+        return dp[n];
+
     }
 };
