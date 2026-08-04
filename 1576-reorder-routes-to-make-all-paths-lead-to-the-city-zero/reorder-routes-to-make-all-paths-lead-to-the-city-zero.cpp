d2eecacc488c0a8fb511d4bf8c6227786d18c6c1
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-08-04 19:29:48 +0530

    Time: 146 ms (27.63%) | Memory: 130.9 MB (17.15%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,27 +1,23 @@
 class Solution {
-    void solve(unordered_map<int, list<int>>& adj, unordered_map<int, list<int>>& real, int& count, int node, int parent){
-        // vis[node] = true;
-        for(auto nbr: real[node]){
-            if(nbr != parent) count++;
-        }
+    void solve(unordered_map<int, list<pair<int, int>>>& adj, int& count, int node, int parent){
         for(auto nbr: adj[node]){
-            if(nbr != parent) solve(adj, real, count, nbr, node);
+            if(nbr.first != parent){
+                if(nbr.second == 1) count++;
+                solve(adj, count, nbr.first, node);
+            }
         }
     }
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
         int count = 0;
-        unordered_map<int, list<int>> adj;
-        unordered_map<int, list<int>> real;
-        // vector<bool> vis(n, 0);
+        unordered_map<int, list<pair<int, int>>> adj;
 
         for(int i=0; i<n-1; i++){
-            adj[connections[i][0]].push_back(connections[i][1]);
-            real[connections[i][0]].push_back(connections[i][1]);
-            adj[connections[i][1]].push_back(connections[i][0]);
+            adj[connections[i][0]].push_back({connections[i][1], 1});
+            adj[connections[i][1]].push_back({connections[i][0], 0}); // fake connection - doesn't exist in real graph
         }
 
-        solve(adj, real, count, 0, -1);
+        solve(adj, count, 0, -1);
         return count;
     }
 };
