e9556f08e4db608f5faf7ace149e66da3d5d69b8
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-08-18 18:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (5.72%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,23 +1,15 @@
 class Solution {
-    bool solve(vector<int>&nums, int s, int e, int score1, int score2, bool turn){
-        if(s > e){
-            if(score1 >= score2) return true;
-            return false;
-        }
+    int solve(vector<int>& piles, int s, int e, vector<vector<int>>& dp){
+        if(s > e) return 0;
+        if(dp[s][e] != -1) return dp[s][e];
 
-        if(turn){
-            bool ans1 = solve(nums, s+1, e, score1 + nums[s], score2, 0);
-            bool ans2 = solve(nums, s, e-1, score1 + nums[e], score2, 0);
-            return ans1 || ans2; // game changer - player1 is playing optimally
-        }
-        else{
-            bool ans1 = solve(nums, s+1, e, score1, score2 + nums[s], 1);
-            bool ans2 = solve(nums, s, e-1, score1, score2 + nums[e], 1);
-            return ans1 && ans2; // game changer - player2 is playing optimally
-        }
+        int start = piles[s] - solve(piles, s+1, e, dp);
+        int end = piles[e] - solve(piles, s, e-1, dp);
+        return dp[s][e] = max(start, end);
     }
 public:
     bool predictTheWinner(vector<int>& nums) {
-        return solve(nums, 0, nums.size()-1, 0, 0, 1);
+        vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), -1));
+        return solve(nums, 0, nums.size()-1, dp) >= 0;
     }
 };
