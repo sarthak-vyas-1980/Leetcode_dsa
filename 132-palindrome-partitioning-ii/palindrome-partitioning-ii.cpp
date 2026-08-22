8176ca1dc11c7fd3fd413f3be9d31cf780371679
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-08-23 01:39:59 +0530

    Time: 58 ms (69.07%) | Memory: 12.3 MB (27.84%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int solve(string& s, int ind, vector<vector<bool>>& palinCheck, vector<int>& dp){
+        if(ind == s.length()) return 0;
+        if(dp[ind] != -1) return dp[ind];
+
+        int ans = INT_MAX;
+        for(int i=ind; i<s.length(); i++){
+            if(palinCheck[ind][i]){
+                ans = min(ans, 1+solve(s, i+1,palinCheck, dp));
+            }
+        }
+        return dp[ind] = ans;
+    }
+public:
+    int minCut(string s) {
+        int n = s.length();
+        vector<int> dp(n, -1);
+        vector<vector<bool>> palinCheck(n, vector<bool>(n, false));
+
+        for(int i=n-1; i>=0; i--){
+            palinCheck[i][i] = true;
+            for(int j=i+1; j<n; j++){
+                if(s[i] == s[j] && (j-i < 2 || palinCheck[i+1][j-1])) palinCheck[i][j] = true; 
+            }
+        }
+        return solve(s, 0, palinCheck, dp) - 1;
+    }
+};
