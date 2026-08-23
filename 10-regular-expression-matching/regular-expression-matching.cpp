2e0c007e8ae34950ba79852f5bda788fa69d7d85
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-08-24 01:54:20 +0530

    Time: 3 ms (46.30%) | Memory: 9.5 MB (23.08%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -17,7 +17,22 @@ class Solution {
     }
 public:
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.length()+1, vector<int>(p.length()+1, -1));
-        return solve(s, p, 0, 0, dp);
+        vector<vector<int>> dp(s.length()+1, vector<int>(p.length()+1, 0));
+        dp[s.length()][p.length()] = true;
+
+        for(int i=s.length(); i>=0; i--){
+            for(int j=p.length()-1; j>=0; j--){
+                bool match = i<s.length() && (s[i] == p[j] || p[j] == '.');
+                
+                if(j+1<p.length() && p[j+1] == '*'){
+                    bool take = false;
+                    if(match) take = dp[i+1][j];
+                    bool notTake = dp[i][j+2];
+                    dp[i][j] = take || notTake;
+                }
+                else if(match) dp[i][j ]= dp[i+1][j+1];
+            }
+        }
+        return dp[0][0];
     }
 };
