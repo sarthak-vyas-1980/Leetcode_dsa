a5b06f3d29c91da59e31ee731b0228fe5790b065
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-08-27 23:48:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (57.51%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -3,8 +3,7 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1, 0);
         for(int i=1; i<=n; i++){
-            if(i%2 == 0) ans[i] = ans[i/2];
-            else ans[i] = ans[i/2] + 1;
+            ans[i] = ans[i/2] + (i&1);
         }
         return ans;
 
