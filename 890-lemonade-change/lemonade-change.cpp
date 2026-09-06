ad3ae6d733c02790870d64ade4a449968ca47fcb
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-09-06 16:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (44.64%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int count5 = 0, count10 = 0;
+
+        for(int bill: bills){
+            if(bill == 5) count5++;
+
+            else if(bill == 10){
+                if(count5 == 0) return false;
+                count10++;
+                count5--;
+            }
+
+            else if(bill == 20){
+                if(count5 > 0 && count10 > 0){
+                    count5--;
+                    count10--;
+                }
+                else if(count5 >= 3){
+                    count5 -= 3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+};
