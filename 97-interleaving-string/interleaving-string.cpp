6c2e6205ef1d6282ce0d02ad6848dffa9d8f7b87
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-09-19 21:15:19 +0530

    Time: 18 ms (16.63%) | Memory: 18.5 MB (15.08%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,29 +1,29 @@
 class Solution {
-    bool solve(string& s1, string& s2, string& s3, int i, int j, int k, vector<vector<vector<int>>>& dp){
-        if(k == s3.length()) return true;
-        if(i == s1.length()){
-            while(j < s2.length()){
-                if(s2[j++] != s3[k++]) return false;
-            }
-            return k == s3.length();   
-        }
-        if(j == s2.length()){
-            while(i < s1.length()){
-                if(s1[i++] != s3[k++]) return false;
-            }
-            return k == s3.length();
-        }
-        if(dp[i][j][k] != -1) return dp[i][j][k];
-
-        bool first = false, second = false;
-        if(s1[i] == s3[k]) first = solve(s1, s2, s3, i+1, j, k+1, dp);
-        if(s2[j] == s3[k]) second = solve(s1, s2, s3, i, j+1, k+1, dp);
-        return dp[i][j][k] = first || second;
-    }
 public:
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.length() + s2.length() != s3.length()) return false;
-        vector<vector<vector<int>>> dp(s1.length(), vector<vector<int>>(s2.length(), vector<int>(s3.length(), -1)));
-        return solve(s1, s2, s3, 0, 0, 0, dp);
+        vector<vector<vector<bool>>> dp(s1.length()+1, vector<vector<bool>>(s2.length()+1, vector<bool>(s3.length()+1, 0)));
+        dp[s1.length()][s2.length()][s3.length()] = true;
+
+        for(int i=s1.length()-1; i>=0; i--){
+            int k = i + s2.length();
+            if(s1[i] == s3[k]) dp[i][s2.length()][k] = dp[i+1][s2.length()][k+1];
+        }
+        for(int j=s2.length()-1; j>=0; j--){
+            int k = j + s1.length();
+            if(s2[j] == s3[k]) dp[s1.length()][j][k] = dp[s1.length()][j+1][k+1];
+        }
+
+        for(int i=s1.length()-1; i>=0; i--){
+            for(int j=s2.length()-1; j>=0; j--){
+                int k = i + j;
+                bool first = false, second = false;
+                if(s1[i] == s3[k]) first = dp[i+1][j][k+1];
+                if(s2[j] == s3[k]) second = dp[i][j+1][k+1];
+                    
+                dp[i][j][k] = first || second;
+            }
+        }
+        return dp[0][0][0];
     }
 };
