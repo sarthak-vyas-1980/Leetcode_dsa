3b8ceaec7e9a6f5ca303753f6e5f33faaad44f0b
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-09-21 01:01:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (23.37%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -2,16 +2,9 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.length() != goal.length()) return false;
-        int size = s.length();
 
-        for(int i=0; i<size; i++){
-            int j = i, ind = 0, count = 0;
-            for(int k=0; k<size; k++){
-                if(s[ind++] == goal[j]) count++;
-                j = (j+1) % size;
-            }
-            if(count == size) return true;
-        }
-        return false;
+        string concatenated = goal + goal;
+        //npos -> no position
+        return concatenated.find(s) != string::npos;
     }
 };
